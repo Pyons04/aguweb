5df862f47ed89af8178e81457f98215a7d974920
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-09-02 22:31:08 +0900

    検索結果画面の修正、絞り込み検索のロジックの修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -7,11 +7,15 @@ class TopController < ApplicationController
     @units=Unit.all
     @units=@units.where('kamoku LIKE(?)',"%#{params[:keyword_kamoku]}%") if params[:keyword_kamoku].present?
     @units=@units.where('kyouin LIKE(?)',"%#{params[:keyword_kyouin]}%").page(params[:page]).per(5) if params[:keyword_kyouin].present?
+
       if params[:day].present?&&params[:jigen].present?
          @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").where('jigen LIKE(?)',"%#{params[:jigen]}%").page(params[:page]).per(5)
-      else
-         @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").page(params[:page]).per(5) if params[:day].present?
-         @units=@units.where('jigen LIKE(?)',"%#{params[:jigen]}%").page(params[:page]).per(5) if params[:jigen].present?
+      elsif params[:day].present?
+         @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").page(params[:page]).per(5)
+      elsif params[:jigen].present?
+         @units=@units.where('jigen LIKE(?)',"%#{params[:jigen]}%").page(params[:page]).per(5)
+        else
+          @units=Unit.all.page(params[:page]).per(5)
       end
   end
 end
