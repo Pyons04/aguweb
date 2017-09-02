4a0341f690fe2f1ff29701cc3206124caf757610
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-09-03 08:51:20 +0900

    .limit(100)をtop_controllerに設置。

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,21 +1,21 @@
 class TopController < ApplicationController
   def index
-    @units=Unit.all.page(params[:page]).per(10)
+    @units=Unit.all.limit(100).page(params[:page]).per(10)
   end
 
   def show
     @units=Unit.all
-    @units=@units.where('kamoku LIKE(?)',"%#{params[:keyword_kamoku]}%") if params[:keyword_kamoku].present?
-    @units=@units.where('kyouin LIKE(?)',"%#{params[:keyword_kyouin]}%").page(params[:page]).per(5) if params[:keyword_kyouin].present?
+    @units=@units.where('kamoku LIKE(?)',"%#{params[:keyword_kamoku]}%").limit(100).page(params[:page]).per(5) if params[:keyword_kamoku].present?
+    @units=@units.where('kyouin LIKE(?)',"%#{params[:keyword_kyouin]}%").limit(100).page(params[:page]).per(5) if params[:keyword_kyouin].present?
 
       if params[:day].present?&&params[:jigen].present?
-         @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").where('jigen LIKE(?)',"%#{params[:jigen]}%").page(params[:page]).per(5)
+         @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").where('jigen LIKE(?)',"%#{params[:jigen]}%").limit(100).page(params[:page]).per(5)
       elsif params[:day].present?
-         @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").page(params[:page]).per(5)
+         @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").limit(100).page(params[:page]).per(5)
       elsif params[:jigen].present?
-         @units=@units.where('jigen LIKE(?)',"%#{params[:jigen]}%").page(params[:page]).per(5)
+         @units=@units.where('jigen LIKE(?)',"%#{params[:jigen]}%").limit(100).page(params[:page]).per(5)
         else
-          @units=Unit.all.page(params[:page]).per(5)
+          @units=Unit.all.limit(100).page(params[:page]).per(5)
       end
   end
 end
