492ef8ff1bd2f4f9dadafc189656344d19c06781
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-09-02 17:45:04 +0900

    絞り込み検索機能の実装,deviceの導入。

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,17 @@
 class TopController < ApplicationController
   def index
+    @units=Unit.all.page(params[:page]).per(10)
+  end
+
+  def show
     @units=Unit.all
+    @units=@units.where('kamoku LIKE(?)',"%#{params[:keyword_kamoku]}%") if params[:keyword_kamoku].present?
+    @units=@units.where('kyouin LIKE(?)',"%#{params[:keyword_kyouin]}%").page(params[:page]).per(5) if params[:keyword_kyouin].present?
+      if params[:day].present?&&params[:jigen].present?
+         @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").where('jigen LIKE(?)',"%#{params[:jigen]}%").page(params[:page]).per(5)
+      else
+         @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").page(params[:page]).per(5) if params[:day].present?
+         @units=@units.where('jigen LIKE(?)',"%#{params[:jigen]}%").page(params[:page]).per(5) if params[:jigen].present?
+      end
   end
 end
