0338d7b9028f76a07c24bcc382cc092ce3188a0c
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-09-03 09:31:13 +0900

    検索システムの不具合の解消、limitの解除。

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -5,17 +5,17 @@ class TopController < ApplicationController
 
   def show
     @units=Unit.all
-    @units=@units.where('kamoku LIKE(?)',"%#{params[:keyword_kamoku]}%").limit(100).page(params[:page]).per(5) if params[:keyword_kamoku].present?
-    @units=@units.where('kyouin LIKE(?)',"%#{params[:keyword_kyouin]}%").limit(100).page(params[:page]).per(5) if params[:keyword_kyouin].present?
+    @units=@units.where('kamoku LIKE(?)',"%#{params[:keyword_kamoku]}%").page(params[:page]).per(5) if params[:keyword_kamoku].present?
+    @units=@units.where('kyouin LIKE(?)',"%#{params[:keyword_kyouin]}%").page(params[:page]).per(5) if params[:keyword_kyouin].present?
 
       if params[:day].present?&&params[:jigen].present?
-         @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").where('jigen LIKE(?)',"%#{params[:jigen]}%").limit(100).page(params[:page]).per(5)
+         @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").where('jigen LIKE(?)',"%#{params[:jigen]}%").page(params[:page]).per(5)
       elsif params[:day].present?
-         @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").limit(100).page(params[:page]).per(5)
+         @units=@units.where('jigen LIKE(?)',"%#{params[:day]}%").page(params[:page]).per(5)
       elsif params[:jigen].present?
-         @units=@units.where('jigen LIKE(?)',"%#{params[:jigen]}%").limit(100).page(params[:page]).per(5)
-        else
-          @units=Unit.all.limit(100).page(params[:page]).per(5)
+         @units=@units.where('jigen LIKE(?)',"%#{params[:jigen]}%").page(params[:page]).per(5)
+      else
+           @units=@units.page(params[:page]).per(5)
       end
   end
 end
