1b535fb2aee37d4270c6b5d68de838e5bdc41feb
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-09-03 10:41:05 +0900

    投稿フォームの作成、redirectができない問題未解決。

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,8 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    Review.create(content:params[:content],test:params[:test],user_id:current_user.id)
+    redirect_to controller: 'reviews', action: 'show'
   end
 
   def destroy
