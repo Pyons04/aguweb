82a1d48fa1dfde90215e58c39e24dcb528ba3bf5
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-09-03 16:54:38 +0900

    戻るボタン（referereメソッドの活用）

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,11 +3,12 @@ class ReviewsController < ApplicationController
   def show
     @reviews=Unit.find(params[:id]).reviews
     @unit=Unit.find(params[:id])
+    @back_url=params[:back_url] if params[:back_url].present?
   end
 
   def create
     Review.create(content:params[:content],satisfaction:params[:satisfaction],test:params[:test],gpa:params[:gpa],user_id:current_user.id,unit_id:params[:unit_id])
-    redirect_to controller: 'reviews', action: 'show', id: params[:unit_id]
+    redirect_to controller: 'reviews', action: 'show', id: params[:unit_id],back_url:params[:back_url]
   end
 
   def destroy
