6545ab22063ec756fcae8ba849727a44d3fe45ac
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-09-03 14:57:17 +0900

    レビューの削除機能の実装（完了）

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,14 +2,18 @@ class ReviewsController < ApplicationController
 
   def show
     @reviews=Unit.find(params[:id]).reviews
+    @unit=Unit.find(params[:id])
   end
 
   def create
-    Review.create(content:params[:content],test:params[:test],user_id:current_user.id)
-    redirect_to controller: 'reviews', action: 'show'
+    Review.create(content:params[:content],satisfaction:params[:satisfaction],test:params[:test],gpa:params[:gpa],user_id:current_user.id,unit_id:params[:unit_id])
+    redirect_to controller: 'reviews', action: 'show', id: params[:unit_id]
   end
 
   def destroy
+    review=Review.find(params[:id])
+    redirect_to controller: 'reviews', action: 'show', id: Review.find(params[:id]).unit.id
+    review.destroy
   end
 
 end
