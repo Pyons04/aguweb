b535728af524e5d9b53fde0e3f2e496138d1d18d
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-09-04 19:01:56 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+require "csv"
+units_csv = CSV.readlines("db/units.csv")
+units_csv.shift
+units_csv.each do |row|
+  Company.create(kyouin:row[1],kamoku:row[2],jigen:row[3],year:row[4],kaikou[5])
+end
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
