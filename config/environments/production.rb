c1d23963d2ff3b4cf6b5d67874bc14ab50b8b9eb
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-09-09 15:01:17 +0900

    メール送信の設定(development)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.raise_runtime_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "broad.basic17@gmail.com", #ご自身のgmailアドレス
+    :password => "takahama", #ご自身のgmailアドレスのパスワード
+    :authentication => 'login',
+  }
 end
