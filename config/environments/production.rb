396366c25d14762ce705f4102cd02f537184a797
COMPUTERNAME\user <broad.basic15@gmail.com>
2017-09-09 16:36:53 +0900

    mail設定setting.mailer.host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.assets.raise_runtime_errors = true
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'Setting.mailer.host'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
